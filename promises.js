f5865019590e38ce27b8403025a6387526a86363
arjun-sudo <arjungautam5431@gmail.com>
2021-03-02 12:30:37 +0545

    Fetch with async , await

--- promises.js
+++ promises.js
@@ -33,13 +33,29 @@ function createPost(post) {
 //     .then(getPosts)
 //     .catch(err => console.log(err));
 
+//Async /Await 
+// async function init(){
+//    await createPost({title:'Post Three',body:'This is post three '})
+//    getPosts();
+// }
+// init()
+
+//Async /Await /Fetch
+async function fetchUser() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
+    console.log(data)
+}
+fetchUser();
+
 //Promise.all
-const promise1 = Promise.resolve('Hello World ');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) =>
-    setTimeout(resolve, 2000, `Goodbye`));
 
-const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+// const promise1 = Promise.resolve('Hello World ');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) =>
+//     setTimeout(resolve, 2000, `Goodbye`));
+
+// const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
 
 
-Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
+// Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
