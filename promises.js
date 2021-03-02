3c99d36a1769a29a10a183ab29bcfe0427a139fd
arjun-sudo <arjungautam5431@gmail.com>
2021-03-02 12:20:47 +0545

    using fet to get the data from the json format using promise

--- promises.js
+++ promises.js
@@ -17,7 +17,7 @@ function createPost(post) {
         setTimeout(() => {
             posts.push(post);
 
-            const error = true;
+            const error = false;
 
             if (!error) {
                 resolve();
@@ -29,6 +29,17 @@ function createPost(post) {
     });
 }
 
-createPost({ title: 'Post Three ', body: 'This is post three ' })
-    .then(getPosts)
-    .catch(err => console.log(err));
+// createPost({ title: 'Post Three ', body: 'This is post three ' })
+//     .then(getPosts)
+//     .catch(err => console.log(err));
+
+//Promise.all
+const promise1 = Promise.resolve('Hello World ');
+const promise2 = 10;
+const promise3 = new Promise((resolve, reject) =>
+    setTimeout(resolve, 2000, `Goodbye`));
+
+const promise4 = fetch('https://jsonplaceholder.typicode.com/users').then(res => res.json());
+
+
+Promise.all([promise1, promise2, promise3, promise4]).then(values => console.log(values));
