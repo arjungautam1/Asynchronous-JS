7def92d446aba61bab8e7deec297d01a183c8739
arjun-sudo <arjungautam5431@gmail.com>
2021-03-02 12:05:39 +0545

    promises

---
+++ promises.js
@@ -0,0 +1,34 @@
+const posts = [
+    { title: 'Post One', body: 'This is post one ' },
+    { title: 'Post Two', body: 'This is post two' }
+];
+
+function getPosts() {
+    setTimeout(() => {
+        let output = '';
+        posts.forEach((post, index) => {
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+function createPost(post) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            posts.push(post);
+
+            const error = true;
+
+            if (!error) {
+                resolve();
+            }
+            else {
+                reject('Error :Something went wrong');
+            }
+        }, 2000)
+    });
+}
+
+createPost({ title: 'Post Three ', body: 'This is post three ' })
+    .then(getPosts)
+    .catch(err => console.log(err));
